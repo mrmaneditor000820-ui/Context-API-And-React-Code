ea1b33cec723e47896fb6f6ed6a6403eee26d810
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-06-05 22:28:31 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Theme } from './context api/Themecontext'
 
 function App() {
+   
+  const {user,setuser} = useContext(Theme)
+  const change = ()=>{
+    setuser({
+      name :"sikander ali",
+      age :"35",
+      email :"sikanderali@gamil.com"
+    })
+  }
+  
   return (
+    
     <>
-    <h1>App</h1>
-    </>
+    <h1>hi i am {user.name}</h1>
+    <h1>And my age is  {user.age}</h1>
+    <h1>And my email is {user.email}</h1>
+    <button style={{border:"2px solid blue",padding:"5px",borderRadius:"20px"}}
+    onClick={change}
+    >Change data</button>
+</>
   )
 }
 
