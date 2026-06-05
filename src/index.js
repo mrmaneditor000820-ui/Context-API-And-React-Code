7a8bbcff982d46c4a4671367ae8ea522aeddda12
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-06-05 22:26:21 +0500

    main function added in index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Usercontaxt from './context api/Themecontext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Usercontaxt>
     <App />
+    </Usercontaxt>
   </React.StrictMode>
 );
 
